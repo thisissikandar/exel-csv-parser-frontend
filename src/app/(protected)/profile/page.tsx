7a89944366f4c52dcc096d6089ecbc7fb920f2e2
Sikandar Chauhan <sikandarc989@gmail.com>
2025-08-25 18:27:59 +0530

    fix

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { api } from "@/lib/api";
+import React, { useEffect, useState } from "react";
 
-const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-  return <div>{children}</div>;
+const page = () => {
+  const [data, setData] = useState<any>(null);
+  useEffect(() => {
+    api
+      .get("/users/profile")
+      .then((res) => {
+        setData(() => res.data.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleLogOut = () => {
+    api.post("/users/logout").then((res)=>{
+      console.log(res);
+      
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
+
+  return (
+    <div className="container mx-auto p-4">
+      {data && (
+        <Card>
+          <div>{data.name}</div>
+          <div>{data.email}</div>
+          <div>{data.role}</div>
+        </Card>
+      )}
+      <Button onClick={handleLogOut}>Logout</Button>
+    </div>
+  );
 };
-
-export default layout;
+export default page;
